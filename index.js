30dc335433537a10a65aa051714a1a0c69ed22ce
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2020-04-14 10:03:44 +0530

    upto crud operation

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = new express();
 
 const path = require('path');
 //const { engine } = require('express-edge');
-const expressEdge = require('express-edge');
+//const expressEdge = require('express-edge');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');  const Post = require('./database/models/Post');
 
-mongoose.connect('mongodb+srv://subhajit:subhajit1234@cluster0-nk2jp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect('mongodb+srv://subhajit:subhajit1234@cluster0-nk2jp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
 
@@ -25,7 +27,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+//see all post
 app.get('/', async (req, res) => {
     const posts = await Post.find({})
     res.send(posts)
@@ -46,8 +48,22 @@ app.get('/post:id', async (req, res) => {
 //console.log(post);
 res.send(post)
 })
-
-
+//delete
+app.delete('/post:id', async (req, res) => {
+     Post.findByIdAndRemove(req.params.id,err => {
+        if (err) return res.send(500, err);
+        
+        })
+        return res.send("success")
+})
+app.put('/post:id', async (req, res) => {
+    Post.findByIdAndUpdate(req.params.id,req.body).then (function() {
+       Post.findById(req.params.id).then (function(post) {
+       res.send(post)
+       })
+       
+})
+});
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 });
